a86fd28adc4de2e5b77cf38066c3462bcdf9d211
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-11-20 11:51:08 +1100

    'initial_commit'

---
+++ Scissors Paper Rock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Scissors_Paper_Rock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerScore = 0;
+            int oppScore = 0;
+
+            Console.WriteLine("Welcome to ROCK PAPER SCISSORS");
+
+            while (playerScore != 3 && oppScore != 3)
+            {
+                Console.WriteLine("Player score - " + playerScore + ". Enemy Score - " + oppScore);
+            }
+        }
+    }
+}
